e9d8a4478e0ddf9083eb16128159a4a137be0eec
Jayden-Siete <jaybookingmanagement@gmail.com>
2026-04-02 12:51:54 +0000

    feat(web): add hooks barrel export - index.ts re-exports all custom hooks and their types

---
+++ apps/web/src/hooks/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Barrel export for all custom React hooks.
+ * Import from "../../hooks" instead of individual files.
+ */
+export { useCliAutoplay } from "./useCliAutoplay";
+export { useCopyToClipboard } from "./useCopyToClipboard";
+export { usePolicyConfig } from "./usePolicyConfig";
+export type { GuardConfig, PolicyConfig } from "./usePolicyConfig";
+export { useSimulation } from "./useSimulation";
+export type { AgentAction, SimResult } from "./useSimulation";
+export { useTheme } from "./useTheme";
